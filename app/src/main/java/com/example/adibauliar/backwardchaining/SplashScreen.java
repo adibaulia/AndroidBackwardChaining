cef451ab3adb20b35a05ae61938160ed97839026
Rohim <adibaulia@gmail.com>
2018-09-18 09:36:36 +0700

    first commit

---
+++ app/src/main/java/com/example/adibauliar/backwardchaining/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.adibauliar.backwardchaining;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1500;
+
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash_screen);
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
