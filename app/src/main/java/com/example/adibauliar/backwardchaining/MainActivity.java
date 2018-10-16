d9303f84bd31efba5af376ac083355184ae738cc
Unknown <adibaulia@gmail.com>
2018-10-16 09:35:39 +0700

    test

--- app/src/main/java/com/example/adibauliar/backwardchaining/MainActivity.java
+++ app/src/main/java/com/example/adibauliar/backwardchaining/MainActivity.java
@@ -11,10 +11,12 @@ import com.github.cschen1205.ess.engine.LessClause;
 import com.github.cschen1205.ess.engine.Rule;
 import com.github.cschen1205.ess.engine.RuleInferenceEngine;
 
+import java.util.Scanner;
 import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
     TextView test;
+    RuleInferenceEngine rie=getInferenceEngine();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,41 +25,39 @@ public class MainActivity extends AppCompatActivity {
         this.getInferenceEngine();
 
         test = (TextView) findViewById(R.id.test);
-        test.setText(testBackwardChain().toString());
+        //test.setText(testBackwardChain().toString());
+        pertanyaan();
 
 
     }
 
-    public Clause testBackwardChain()
+//    public Clause testBackwardChain()
+//    {
+//
+//        rie.addFact(new EqualsClause("zat", "cream"));
+//        rie.addFact(new EqualsClause("warna", "berwarna"));
+//        rie.addFact(new EqualsClause("kegunaan", "untuk make up"));
+//        rie.addFact(new EqualsClause("kegunaan", "di bibir"));
+//
+//        Vector<Clause> unproved_conditions= new Vector<>();
+//
+//        Clause conclusion=rie.infer("type", unproved_conditions);
+//
+//        return(conclusion);
+//    }
+
+    public void pertanyaan()
     {
-        RuleInferenceEngine rie=getInferenceEngine();
-        rie.addFact(new EqualsClause("num_wheels", "4"));
-        rie.addFact(new EqualsClause("motor", "yes"));
-        rie.addFact(new EqualsClause("num_doors", "3"));
-        rie.addFact(new EqualsClause("size", "medium"));
-
-        System.out.println("Infer: vehicle");
-
-        Vector<Clause> unproved_conditions= new Vector<>();
-
-        Clause conclusion=rie.infer("vehicle", unproved_conditions);
-
-        return(conclusion);
+        rie.getRules();
+        System.out.println(rie.getRule(1).nextAntecedent());
     }
+
 
     private RuleInferenceEngine getInferenceEngine()
     {
         RuleInferenceEngine rie=new KieRuleInferenceEngine();
 
-        Rule rule=new Rule("Miceral Water");
-        rule.addAntecedent(new EqualsClause("zat", "cair"));
-        rule.addAntecedent(new EqualsClause("warna", "transparan"));
-        rule.addAntecedent(new EqualsClause("sifat", "tidak membuat iritasi"));
-        rule.addAntecedent(new EqualsClause("kandungan", "mengandung coconut oil"));
-        rule.setConsequent(new EqualsClause("type", "Miceral Water"));
-        rie.addRule(rule);
-
-        rule=new Rule("Lip Cream");
+        Rule rule=new Rule("Lip Cream");
         rule.addAntecedent(new EqualsClause("zat", "cream"));
         rule.addAntecedent(new EqualsClause("warna", "berwarna"));
         rule.addAntecedent(new EqualsClause("kegunaan", "untuk make up"));
@@ -65,20 +65,28 @@ public class MainActivity extends AppCompatActivity {
         rule.setConsequent(new EqualsClause("type", "Lip Cream"));
         rie.addRule(rule);
 
-        rule=new Rule("Lip Cream");
+        rule=new Rule("Mascara");
         rule.addAntecedent(new EqualsClause("zat", "cream"));
         rule.addAntecedent(new EqualsClause("warna", "berwarna"));
         rule.addAntecedent(new EqualsClause("kegunaan", "untuk make up"));
         rule.addAntecedent(new EqualsClause("kegunaan", "di bibir"));
-        rule.setConsequent(new EqualsClause("type", "Lip Cream"));
+        rule.setConsequent(new EqualsClause("type", "Mascara"));
         rie.addRule(rule);
 
-        rule=new Rule("Lip Cream");
+        rule=new Rule("Makeup Remover");
         rule.addAntecedent(new EqualsClause("zat", "cream"));
         rule.addAntecedent(new EqualsClause("warna", "berwarna"));
         rule.addAntecedent(new EqualsClause("kegunaan", "untuk make up"));
         rule.addAntecedent(new EqualsClause("kegunaan", "di bibir"));
-        rule.setConsequent(new EqualsClause("type", "Lip Cream"));
+        rule.setConsequent(new EqualsClause("type", "Makeup Remover"));
+        rie.addRule(rule);
+
+        rule=new Rule("BB cream");
+        rule.addAntecedent(new EqualsClause("zat", "cream"));
+        rule.addAntecedent(new EqualsClause("warna", "berwarna"));
+        rule.addAntecedent(new EqualsClause("kegunaan", "untuk make up"));
+        rule.addAntecedent(new EqualsClause("kegunaan", "di bibir"));
+        rule.setConsequent(new EqualsClause("type", "BB cream"));
         rie.addRule(rule);
 
         return rie;
